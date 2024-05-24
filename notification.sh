dd4781021769a8d4401d36e80b279a0e09efaaef
スジャん <111575677+Juki-Sogajan@users.noreply.github.com>
2024-05-24 14:52:46 +0900

    first commit

---
+++ notification.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+# MyDNS 通知用すくりぷとすくりぷと
+
+
+# 設定用変数
+
+WORKING_DIR="."
+
+URL_IPV4=https://ipv4.mydns.jp/login.html
+URL_IPV6=https://ipv6.mydns.jp/login.html
+
+ID=""
+PASSWORD=""
+
+
+# メイン処理
+
+cd $WORKING_DIR
+
+wget --http-user=$ID --http-passwd=$PASSWORD -O ipv4.html $URL_IPV4
+wget --http-user=$ID --http-passwd=$PASSWORD -O ipv6.html $URL_IPV6
